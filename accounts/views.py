44b14e78494d43e9476d2bad36550b26e76b36c1
Mariusz Duda <mariusz.z.duda@gmail.com>
2020-08-31 15:55:57 +0200

    update and remove items function

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 from .models import *
+from .forms import OrderForm
 
 
 def home(request):
@@ -41,3 +42,37 @@ def customer(request, pk_test):
         'orders_count': orders_count
     }
     return render(request, 'accounts/customer.html', context)
+
+
+def create_order(request):
+    form = OrderForm()
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def update_order(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def delete_order(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+    context = {"item": order}
+    return render(request, 'accounts/delete.html', context)
